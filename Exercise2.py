22a70a07029548e3802b9958d736eb0a2add4ab9
AbigailLouiseDutton <abigailloudutton@gmail.com>
2019-01-05 21:33:46 +0000

    Create Exercise2.py

---
+++ Exercise2.py
@@ -0,0 +1,12 @@
+number = int(input("Please type in a number:"))
+mod = number % 2
+if mod > 0:
+	print("Your number was odd")
+else:
+	print("Your number was even")
+
+#Often it is useful for us to find not the answer to a division problem but the remainder when we do a division operation. 
+
+#This operation is called the “modulus operation.” 
+
+#For example, when I divide 5 by 3, the remainder is 2, and the sentence reads like this: “5 modulo 3 is 2.” = 5 % ==2
